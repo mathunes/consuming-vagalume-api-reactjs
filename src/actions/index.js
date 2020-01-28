c225cd09a108d42a6201dc19baba41d7ffea0457
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-28 17:40:23 -0300

    Adicao da action para requisitar o ranking

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,16 @@
 import api from "../services/api";
 
-export function searchRankingFive() {
+export function searchRanking(num = 5) {
     return dispatch => {
-        // dispatch(searchRankingFiveStarted());
+        dispatch(searchRankingStarted());
 
-        // api.get(`rank.php?type=mus,alb,art&&scope=all&limit=5&apikey={key}`)
+        api.get(`rank.php?type=mus,alb,art&&scope=all&limit=${num}&apikey={key}`)
+        .then(response => {
+            dispatch(searchRankingSuccess(response.data));
+        })
+        .catch(error => {
+            dispatch(searchRankingFailure(error.message));
+        })
     }
 }
 
@@ -21,6 +27,22 @@ export function searchMusicArtist(text) {
         })
     }
 }
+
+const searchRankingStarted = () => ({
+    type: 'SEARCH_RANKING_STARTED',
+})
+
+const searchRankingSuccess = (data) => ({
+    type: 'SEARCH_RANKING_SUCCESS',
+    data
+})
+
+const searchRankingFailure = (error) => ({
+    type: 'SEARCH_RANKING_SUCCESS',
+    payload: {
+        error
+    }
+})
 
 const searchMusicArtistStarted = () => ({
     type: 'SEARCH_MUSIC_ARTIST_STARTED',
