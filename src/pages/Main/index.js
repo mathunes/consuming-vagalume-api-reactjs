775ddff663cbf4cbff07682733b5f9ecbd7c9ad2
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-28 17:54:11 -0300

    Conectando main as acoes

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as searchActions from '../../actions';
 
 class Main extends Component {
     render() {
@@ -18,4 +20,7 @@ const mapStateToProps = state => ({
     search: state.search
 })
 
-export default connect(mapStateToProps)(Main);
+const mapDispatchToProps = dispatch =>
+    bindActionCreators(searchActions, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
