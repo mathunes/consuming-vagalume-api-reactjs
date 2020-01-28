e2d068678aa637ba7146d74bf67e77eb7fa6f518
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-28 19:25:24 -0300

    Exibicao do dados do rank no main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -10,20 +10,59 @@ class Main extends Component {
     }
 
     render() {
-        console.log(this.props.search);
-        console.log(this.props.search.dataRanking);
+
+        let container;
+
+        if (this.props.search.searchRanking) {
+            let {mus, art, alb} = this.props.search.dataRanking;
+
+            container = 
+                <div className="container">
+                    <div className="card-music">
+                        <h3>Músicas</h3>
+                        <ol>
+                        {(mus.month.all).map((item, i) => {
+                            return (
+                                <li key={i}>
+                                    <p>{item.name}</p>
+                                    <p>{item.art.name}</p>
+                                </li>
+                            )
+                        })}
+                        </ol>
+                    </div>
+                    <div className="card-artist">
+                        <h3>Artistas</h3>
+                        <ol>
+                        {(art.month.all).map((item, i) => {
+                            return (
+                                <li key={i}>
+                                    <p>{item.name}</p>
+                                </li>
+                            )
+                        })}
+                        </ol>
+                    </div>
+                    <div className="card-album">
+                        <h3>Álbum</h3>
+                        <ol>
+                        {(alb.month.all).map((item, i) => {
+                            console.log(item)
+                            return (
+                                <li key={i}>
+                                    <p>{item.name}</p>
+                                    <p>{item.art.name}</p>
+                                </li>
+                            )
+                        })}
+                        </ol>
+                    </div>
+                </div>
+        }
         
         return (
             <main>
-                <div className="card-music">
-
-                </div>
-                <div className="card-artist">
-
-                </div>
-                <div className="card-album">
-
-                </div>
+                {container}
             </main>
         )
     }
