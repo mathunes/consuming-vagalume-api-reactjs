28c482c4c3cf8a3fe053ff328a8cbf7ee84961cd
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-28 21:49:24 -0300

    Conexao da pagina do Top100 com o store

--- src/pages/Top100/index.js
+++ src/pages/Top100/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Top100 extends Component {
+class Top100 extends Component {
+    componentDidMount() {
+        console.log(this.props);
+    }
+
     render() {
         return (
-            <p>OI</p>
+            <p>Teste</p>
         )
     }
 }
+
+const mapStateToProps = state => ({
+    search: state.searchRanking,
+})
+
+export default connect(mapStateToProps)(Top100);
