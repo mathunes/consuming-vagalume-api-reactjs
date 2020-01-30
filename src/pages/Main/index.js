cba98362e8d186ceb9ef6ae07e72ca707183cc7d
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-30 14:16:44 -0300

    Adicao do estilo ao titulo e alinhamento dos cards

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux';
 import * as searchActions from '../../actions';
 import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
-import { MainContainer } from './styles';
+import { MainContainer, CardsContainer, CardContainer } from './styles';
 
 class Main extends Component {
 
@@ -20,8 +20,8 @@ class Main extends Component {
             let {mus, art, alb} = this.props.search.dataRanking;
 
             container = 
-                <div className="container">
-                    <div className="card-music">
+                <CardsContainer>
+                    <CardContainer>
                         <h3>Músicas</h3>
                         <ol>
                         {(mus.month.all).map((item, i) => {
@@ -34,8 +34,8 @@ class Main extends Component {
                         })}
                         </ol>
                         <Link to="/top100/musics">Ver o top 100 músicas</Link>
-                    </div>
-                    <div className="card-artist">
+                    </CardContainer>
+                    <CardContainer>
                         <h3>Artistas</h3>
                         <ol>
                         {(art.month.all).map((item, i) => {
@@ -47,8 +47,8 @@ class Main extends Component {
                         })}
                         </ol>
                         <Link to="/top100/artists">Ver o top 100 artistas</Link>
-                    </div>
-                    <div className="card-album">
+                    </CardContainer>
+                    <CardContainer>
                         <h3>Álbum</h3>
                         <ol>
                         {(alb.month.all).map((item, i) => {
@@ -61,15 +61,18 @@ class Main extends Component {
                         })}
                         </ol>
                         <Link to="/top100/albums">Ver o top 100 albuns</Link>
-                    </div>
-                </div>
+                    </CardContainer>
+                </CardsContainer>
         }
         
         return (
             <MainContainer>
                 <Header />
                 <main>
-                    <h1>Top 100</h1>
+                    <h1>
+                        Top 100
+                        <span>As músicas, álbuns e artistas mais acessados</span>
+                    </h1>
                     {container}
                 </main>
             </MainContainer>
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -9,17 +9,34 @@ export const MainContainer = styled.div`
     main {
         width: 100%;
         padding: 20px;
+        box-sizing: border-box;
         font-family: 'Rubik', sans-serif;
 
         h1 {
             text-align: center;
-            font-weight: bold;
-        }
-
-        .container {
+            font-weight: bolder;
+            font-size: 24px;
+            text-transform: uppercase;
+            color: #2d2d2d;
             display: flex;
-            flex-direction: row;
-            justify-content: space-around
+            flex-direction: column;
+
+            span {
+                font-weight: 200;
+                font-size: 14px;
+                color: #737373;
+                margin: 10px;
+            }
         }
     }
 `;
+
+export const CardsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around
+`;
+
+export const CardContainer = styled.div`
+    background-color: #c4c4c4;
+`
