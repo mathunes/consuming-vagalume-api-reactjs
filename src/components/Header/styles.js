7177381fd96eac5729d68e63f6cf3b94fca52fa4
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-30 09:24:18 -0300

    Header responsiva

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -17,6 +17,7 @@ export const HeaderContainer = styled.header`
         align-items: center;
         border-radius: 5px;
         transition: all .2s;
+        position: relative;
 
         &:hover {
             box-shadow: 0px 0px 15px 2px #CCFF00;
@@ -44,6 +45,7 @@ export const HeaderContainer = styled.header`
 
         input[type="text"] {
             background-color: #333333;
+            color: #ffffff;
             padding: 15px;
             box-sizing: border-box;
             border: 0;
@@ -78,4 +80,44 @@ export const HeaderContainer = styled.header`
 
     }
 
+    @media (max-width: 700px) {
+        img {
+            margin: 0 auto;
+            width: 150px;
+        }
+
+        form {
+            input[type="text"] {
+                display: none;
+            }
+
+            &:hover {
+                box-shadow: none;
+            }
+
+            button {
+                background-color: #151614;
+                position: absolute;
+                padding-right: 20px;
+                right: 0;
+
+                img {
+                    width: 20px;
+                }
+            }
+
+        }
+    }
+
+    @media (max-width: 400px) {
+        img {
+            width: 130px;
+        }
+
+        form {
+            button {
+                padding-right: 10px;
+            }
+        }
+    }
 `;
