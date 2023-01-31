484c2135a698ec10169adca173b52b9877840fe8
Matheus Vieira <matheus.vieira@primeup.com.br>
2023-01-31 02:19:59 -0300

    Updates

---
+++ precache-manifest.bccf6062ff0eda586bfbaee9a12c5fee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5798cdd4e0f5e329cefbed91a9b3c177",
+    "url": "/vagalume-clone/index.html"
+  },
+  {
+    "revision": "3d6704cb0128db62b8d1",
+    "url": "/vagalume-clone/static/css/main.2acfe684.chunk.css"
+  },
+  {
+    "revision": "e28d9b91972c1ca6b1e0",
+    "url": "/vagalume-clone/static/js/2.599ab5ed.chunk.js"
+  },
+  {
+    "revision": "1932a00fd6235f2803dd3f13c0ec9af5",
+    "url": "/vagalume-clone/static/js/2.599ab5ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d6704cb0128db62b8d1",
+    "url": "/vagalume-clone/static/js/main.0be3282b.chunk.js"
+  },
+  {
+    "revision": "0e77941693c9473d00e7",
+    "url": "/vagalume-clone/static/js/runtime-main.0fc7fd14.js"
+  },
+  {
+    "revision": "d37456205c2eedbac1b901f89dbbfa3a",
+    "url": "/vagalume-clone/static/media/loading.d3745620.gif"
+  },
+  {
+    "revision": "10b9b6a3759feb0519e56f77180efc38",
+    "url": "/vagalume-clone/static/media/logo.10b9b6a3.png"
+  }
+]);
